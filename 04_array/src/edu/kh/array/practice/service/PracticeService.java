14cf89b321f51e4af5a86af112185de1433ae391
Wook-J <dnrwo0827@naver.com>
2024-09-04 01:19:07 +0900

    240904 오전1시19분

--- 04_array/src/edu/kh/array/practice/service/PracticeService.java
+++ 04_array/src/edu/kh/array/practice/service/PracticeService.java
@@ -219,7 +219,90 @@ public class PracticeService {
 		for(int i=0; i<arr.length; i++) System.out.print(arr[i] + " ");
 	}
 	
-	public void practice13() {
+    public void practice13() {
+    	
+    	System.out.print("문자열 : ");
+    	String str = sc.nextLine();
+
+    	char[] arr = new char[str.length()];
+
+    	for(int i=0; i<arr.length; i++) arr[i] = str.charAt(i);
+
+    	int count = 0;					// 문자 개수 카운트
+
+    	System.out.print("문자열에 있는 문자 : ");
+
+    	for(int i=0; i<arr.length; i++) {
+
+    		boolean flag = true;		// 중복 체크용 flag
+
+    		for(int j=0; j<i; j++) {
+    			if(arr[i] == arr[j]) {
+    				flag = false;		// 중복이 발생 시
+    				break;
+    			}
+    		}
+
+    		if(flag) { 					// 중복이 발생 X인 경우
+
+    			if(i == 0) System.out.print(arr[i]);	// 0번째 무조건 true
+    			else System.out.print(", " + arr[i]);
+
+    			count++;				// 중복 아닐때만 문자 개수 증가
+    		}
+    	}
+    	System.out.println("\n문자 개수 : " + count);
+    }
+	
+    public void practice14() {
+
+    	// 1. 첫 배열 크기 지정
+    	System.out.print("배열의 크기를 입력하시오 : ");
+    	int size = sc.nextInt();
+    	sc.nextLine();							// nextLine() 사용 시 유의사항
+
+    	String[] arr = new String[size];
+
+        for(int i=0; i<arr.length ; i++) {		// 2. 첫 배열에 저장할 문자열 입력 받기
+        	System.out.print((i+1) + "번째 문자열 : ");
+            arr[i] = sc.nextLine();
+        }
+
+        while(true) {	// 3. 반복이 시작되는 구간부터 무한루프로 작성하여 내부에 종료 조건을 만들어 break
+        	System.out.print("더 값을 입력하시겠습니까?(Y/N) : ");
+            char ch = sc.nextLine().charAt(0);
+
+            if(ch == 'y' || ch == 'Y') {		// 4. 값을 더 입력할 경우
+                System.out.print("더 입력하고 싶은 개수 : ");		// 5. 더 입력받을 개수 입력 받기
+                int addSize = sc.nextInt();
+                sc.nextLine();
+
+                // 6. 새로 값을 입력 받을 배열 생성 --> 기존 배열 크기 + 추가 입력 개수
+                String[] newArr = new String[arr.length + addSize];
+
+                // 7. 배열 복사 + 새로운 문자열 입력 받기
+                for(int i=0; i<newArr.length ; i++) {
+                    if(i<arr.length) { // 인덱스의 크기가 기존 배열보다 작을 경우 기존 배열값 복사
+                        newArr[i] = arr[i];
+                    }else { // 인덱스의 크기가 기존 배열보다 클 경우 새로운 문자열 입력 받기
+                        System.out.print((i+1) + "번째 문자열 : ");
+                        newArr[i] = sc.nextLine();
+                    }
+                }
+
+                // 8. 기존 배열공간을 참조하던 변수 arr에 새로운 배열 공간의 주소 newArr 대입
+                arr = newArr;
+
+            }else if(ch == 'n' || ch == 'N'){ // 9. 값을 더 입력하지 않은 경우
+                break; // 반복문 종료
+            }else { // 잘못 입력한 경우
+                System.out.println("잘못 입력하셨습니다. 다시 입력해 주세요.");
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    
+	public void practice13Mang() {
 		System.out.printf("문자열 : ");
 		String str = sc.nextLine();
 		
@@ -258,7 +341,7 @@ public class PracticeService {
 		System.out.println("\n문자 개수 : "+countTure);
 	}
 
-	public void practice14() {
+	public void practice14Mang() {
 		/*
 		 * 사용자가입력한배열의길이만큼의문자열배열을선언및할당하고
 배열의인덱스에넣을값역시사용자가입력하여초기화하세요.
@@ -295,7 +378,6 @@ public class PracticeService {
 		while(true) {
 			System.out.print("더 값을 입력하시겠습니까?(Y/N)");
 			String YesOrNo = sc.next();
-			String[] arrNew;
 			
 			if(YesOrNo.equals("Y") || YesOrNo.equals("y")) {
 				
